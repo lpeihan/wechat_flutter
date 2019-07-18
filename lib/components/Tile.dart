2f5c533e9b7690779f579e1330c994212f773ea0
李培汉 <lipeihan@chandashi.com>
2019-07-18 22:47:21 +0800

    feat: 发现页面

---
+++ lib/components/Tile.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:wechat_flutter/constants/app_colors.dart';
+
+class Tile extends StatelessWidget {
+  final String iconPath;
+  final String title;
+  final bool marginBottom;
+  final bool borderBottom;
+
+  const Tile({Key key, this.iconPath, this.title, this.marginBottom = false, this.borderBottom = false}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        color: Colors.white,
+        margin: EdgeInsets.only(bottom: marginBottom ? 12.0 : 0.0),
+        padding: EdgeInsets.only(left: 15.0),
+        child: Row(
+          children: <Widget>[
+            Image.asset(iconPath, width: 30.0,),
+            SizedBox(width: 15.0,),
+            Expanded(
+              child: Container(
+                height: 55.0,
+                alignment: Alignment.centerLeft,
+                child: Text(title, style: TextStyle(fontSize: 16.0),),
+                decoration: BoxDecoration(
+                  border: borderBottom ? Border(bottom: BorderSide(width: 0.5, color: Color(AppColors.borderColor))) : Border()
+                ),
+              )
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/discover_page.dart
+++ lib/pages/discover_page.dart
@@ -1,12 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:wechat_flutter/components/Tile.dart';
+import 'package:wechat_flutter/constants/app_colors.dart';
 
 class DiscoverPage extends StatelessWidget {
   const DiscoverPage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('DiscoverPage'),
+    return  Container(
+      color: Color(AppColors.primaryColor),
+      child: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                Tile(iconPath: 'assets/images/ic_social_circle.png', title: '朋友圈', marginBottom: true,),
+
+                Tile(iconPath: 'assets/images/ic_quick_scan.png', title: '扫一扫', borderBottom: true,),
+                Tile(iconPath: 'assets/images/ic_shake_phone.png', title: '摇一摇', marginBottom: true),
+
+                Tile(iconPath: 'assets/images/ic_feeds.png', title: '看一看', borderBottom: true,),
+                Tile(iconPath: 'assets/images/ic_quick_search.png', title: '搜一搜', marginBottom: true),
+
+                Tile(iconPath: 'assets/images/ic_people_nearby.png', title: '附近的人', borderBottom: true,),
+                Tile(iconPath: 'assets/images/ic_bottle_msg.png', title: '漂流瓶', marginBottom: true),
+
+
+                Tile(iconPath: 'assets/images/ic_shopping.png', title: '购物', borderBottom: true,),
+                Tile(iconPath: 'assets/images/ic_game_entry.png', title: '游戏', marginBottom: true),
+
+                Tile(iconPath: 'assets/images/ic_mini_program.png', title: '小程序', marginBottom: true),
+              ],
+            )
+          ]
+        ),
+      )
     );
   }
 }
