08bfee22525705860c5afbdb82acc60834bcd5c2
李培汉 <lipeihan@chandashi.com>
2019-07-18 18:19:00 +0800

    perf: 通讯录滚动

--- lib/pages/contact_page.dart
+++ lib/pages/contact_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:wechat_flutter/constants/app_colors.dart';
-import 'package:wechat_flutter/models/contacts.dart';
+import 'package:wechat_flutter/models/contacts.dart' show contacts, Contact, INDEX_LETTERS;
 
+const double TITLE_HEIGHT = 24;
+const double ITEM_HEIGHT = 64;
+const double BORDER_WIDTH = 0.5;
 
 class ContactItem extends StatelessWidget {
   final bool isGroupTitle;
@@ -19,8 +22,8 @@ class ContactItem extends StatelessWidget {
 
     if (isGroupTitle) {
       _buildWidget.add(
-       Container(
-          height: 24.0,
+        Container(
+          height: TITLE_HEIGHT,
           padding: EdgeInsets.only(left: 15.0),
           color: Color(AppColors.primaryColor),
           alignment: Alignment.centerLeft,
@@ -47,12 +50,12 @@ class ContactItem extends StatelessWidget {
             Expanded(
               child: Container(
                 alignment: Alignment.centerLeft,
-                height: 64.0,
+                height: ITEM_HEIGHT,
                 margin: EdgeInsets.only(left: 15.0),
                 child: Text(contact.name),
                 decoration: BoxDecoration(
                   border: Border(
-                    bottom: BorderSide(width: 0.5, color: Color(AppColors.borderColor))
+                    bottom: BorderSide(width: BORDER_WIDTH, color: Color(AppColors.borderColor))
                   )
                 ),
               ),
@@ -76,6 +79,9 @@ class ContactPage extends StatefulWidget {
 
 class _ContactPageState extends State<ContactPage> {
   List<Contact> _contacts = [];
+  ScrollController _scrollController = ScrollController();
+  final Map heightMap = { INDEX_LETTERS[0]: 0.0 };
+  double tileHeight = 0; 
 
   List<Contact> topItems = [
     Contact(
@@ -100,39 +106,126 @@ class _ContactPageState extends State<ContactPage> {
     ),
   ];
 
+  GlobalKey indexBarKey = GlobalKey();
+
   @override
   void initState() {
     super.initState();
-  
 
     _contacts
       ..addAll(contacts)
       ..addAll(contacts)
       ..addAll(contacts)
       ..sort((a, b) => a.nameIndex.compareTo(b.nameIndex));
+
+    this.initHeight();
   }
 
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  initHeight() {
+    double _totalHeight = 4 * ITEM_HEIGHT;
+  
+    for(int i = 0; i < _contacts.length; i++) {
+      bool isGroupTitle = true;
+      double height = ITEM_HEIGHT + BORDER_WIDTH;
+
+      if (i >= 1 && _contacts[i].nameIndex.compareTo(_contacts[i - 1].nameIndex) == 0) {
+        isGroupTitle = false;
+      }
+
+      if (isGroupTitle) {
+        height += TITLE_HEIGHT;
+        heightMap[_contacts[i].nameIndex] = _totalHeight;
+      }
+
+      _totalHeight += height;
+    }
+  }
+
+  scrollTo(double pos) {
+    _scrollController.animateTo(pos,
+      curve: Curves.easeOut, duration: Duration(milliseconds: 200));
+  }
+
+  getPos(dy) {
+    int index = (dy ~/ tileHeight).clamp(0, INDEX_LETTERS.length - 1);
+
+    double pos = heightMap[INDEX_LETTERS[index]];
+
+    if (pos != null) {
+      scrollTo(pos);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: _contacts.length,
-      itemBuilder: (BuildContext context, int index) {
-        Contact contact = _contacts[index];
+    return Stack(
+      children: <Widget>[
+        ListView.builder(
+          controller: _scrollController,
+          itemCount: _contacts.length + topItems.length,
+          itemBuilder: (BuildContext context, int index) {
+            Contact contact;
 
-        bool isGroupTitle = true;
+            bool isGroupTitle = true;
 
-        if (index >= 1 && contact.nameIndex == _contacts[index - 1].nameIndex) {
-          isGroupTitle = false;
-        }
+            if (index < topItems.length) {
+              isGroupTitle = false;
+              contact = topItems[index];
+            } else {
+              index = index - topItems.length;
+              contact = _contacts[index];
 
-        if (index < topItems.length) {
-          isGroupTitle = false;
-          contact = topItems[index];
-        }
+              if (index >= 1 && contact.nameIndex == _contacts[index - 1].nameIndex) {
+                isGroupTitle = false;
+              }
+            }
 
-        return ContactItem(contact: contact, isGroupTitle: isGroupTitle);
-      },
+            return ContactItem(contact: contact, isGroupTitle: isGroupTitle);
+          },
+        ),
+        Positioned(
+          width: 30.0,
+          top: 20.0,
+          key: indexBarKey,
+          
+          right: 0.0,
+          bottom: 20.0,
+          child: GestureDetector(
+            onVerticalDragDown: (DragDownDetails details) {
+              if (tileHeight == 0) {
+                RenderObject renderObject = indexBarKey.currentContext.findRenderObject();
+
+                double height = renderObject.semanticBounds.size.height;
+                tileHeight = height / INDEX_LETTERS.length;
+              }
+
+              getPos(details.localPosition.dy);
+            },
+            onVerticalDragEnd: (DragEndDetails details) {
+              print('dragend');
+            },
+            onVerticalDragCancel: () {
+              print('dragcancel');
+            },
+            onVerticalDragUpdate: (DragUpdateDetails details) {
+              getPos(details.localPosition.dy);
+            },
+            child: Column(
+              children: INDEX_LETTERS.map((String letter) {
+                return Expanded(
+                  child: Text(letter, style: TextStyle(fontSize: 12.0),),
+                );
+              }).toList(),
+            ),
+          )
+        )
+      ],
     );
   }
 }
